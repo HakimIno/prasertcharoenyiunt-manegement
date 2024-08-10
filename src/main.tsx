e824062ff68f7b08089344ea2ff4602117fa7b29
weerachit utnok <weerachit.utnok01@gmail.com>
2024-08-10 19:17:39 +0700

    fix: error

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
 import "./index.css"
 import React from 'preact/compat';
-import Router from './router';
+import Router from './Router';
 import { AuthProvider } from './context/AuthContext';
 import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css';
-
 
 const rootElement = document.getElementById('app');
 if (rootElement) {
