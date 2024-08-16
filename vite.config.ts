e5e7d616c595f270777d2ce3e9b30ce82d6b9db0
weerachit utnok <weerachit.utnok01@gmail.com>
2024-08-16 15:12:55 +0700

    fix: 404

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [preact()],
+  base: './', // ตรวจสอบให้แน่ใจว่า base path ถูกต้อง
+  build: {
+    outDir: 'dist',
+  },
 })
